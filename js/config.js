3cd0fdcf8b9e67182c1e4257f3ea22806921bed7
Matt Ray <100228399+Cedaring-xr@users.noreply.github.com>
2025-04-09 09:05:35 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_nU2dnvqDV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4iafdkls73nh0o1ck2n2apb4hd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_f8LgLRN52', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4biumq4ld2ugp56u7jtmjsk5uq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
